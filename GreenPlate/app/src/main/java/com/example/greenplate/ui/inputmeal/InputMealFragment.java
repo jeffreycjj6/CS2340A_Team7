e9c7e4afaaba5df957a119d8301751c15336b9f0
jeffreycjj6 <jeffreycjj6@gmail.com>
2024-03-11 18:46:46 -0400

    Added Data Button functionality for fragments.

--- GreenPlate/app/src/main/java/com/example/greenplate/ui/inputmeal/InputMealFragment.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/ui/inputmeal/InputMealFragment.java
@@ -4,32 +4,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+import com.example.greenplate.R;
 import com.example.greenplate.databinding.FragmentInputMealBinding;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class InputMealFragment extends Fragment {
     private FragmentInputMealBinding binding;
+    private View root;
+
+    private Button data;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         InputMealViewModel inputMealViewModel =
                 new ViewModelProvider(this).get(InputMealViewModel.class);
 
         binding = FragmentInputMealBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        root = binding.getRoot();
 
         TextView textView = binding.userInfo;
         inputMealViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         String meal = Objects.requireNonNull(binding.meal.getText()).toString();
         String calorieCount = Objects.requireNonNull(binding.calorieCount.getText()).toString();
+
+        //data = root.findViewById(R.id.dataButton);
+        Button dataButton = binding.dataButton;
+        dataButton.setOnClickListener(v -> {
+            DataVisualFragment chartFragment = new DataVisualFragment();
+            FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, chartFragment);
+            transaction.addToBackStack(null);  // This line allows the user to navigate back to the InputMealFragment by pressing the back button.
+            transaction.commit();
+        });
 
 
         return root;
