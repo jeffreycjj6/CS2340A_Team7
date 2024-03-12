3f8366d927f264c4f4759b2bf984b0a5b03dc277
Jayden-Causey <jcausey8@gatech.edu>
2024-03-12 17:19:18 -0400

    added getters to input meal frag

--- GreenPlate/app/src/main/java/com/example/greenplate/ui/inputmeal/InputMealViewModel.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/ui/inputmeal/InputMealViewModel.java
@@ -7,13 +7,15 @@ import androidx.lifecycle.ViewModel;
 public class InputMealViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
-    double W = 0;
-    double H = 0;
-    double target = 370 + 21.6 * (1 - (W / (Math.pow(H, 2))) * W);
+    User user = User.getInstance();
+    double target = 370 + 21.6 * (1 - (user.getWeight() / (Math.pow(user.getHeight(), 2)))
+            * user.getWeight());
+
     public InputMealViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("At " + H + "meters tall and " + H + "kilograms, your goal is " + target +
-                " calories.");
+        mText.setValue("At " + user.getHeight() + " meters tall and " + user.getWeight()
+                + " kilograms, your goal is " + target + " calories. You are at "
+                + user.getDailyCalories() + " calories.");
     }
 
     public LiveData<String> getText() {
