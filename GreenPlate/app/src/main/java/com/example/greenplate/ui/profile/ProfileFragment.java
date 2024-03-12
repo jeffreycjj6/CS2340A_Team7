3e171440c4dbf5e6e65e12b4eea16f85923f73b9
htang0641 <ht0641ht@gatech.edu!>
2024-03-12 03:10:59 -0400

    Fixed profile screen and added edit fragment

--- GreenPlate/app/src/main/java/com/example/greenplate/ui/profile/ProfileFragment.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/ui/profile/ProfileFragment.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.greenplate.R;
 import com.example.greenplate.databinding.FragmentProfileBinding;
+import com.example.greenplate.ui.inputmeal.DataVisualFragment;
 
 public class ProfileFragment extends Fragment {
 
@@ -24,8 +28,16 @@ public class ProfileFragment extends Fragment {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textProfile;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Button dataButton = binding.editButton;
+        dataButton.setOnClickListener(v -> {
+            EditStatsFragment editStatsFragment = new EditStatsFragment();
+            FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, editStatsFragment);
+            transaction.addToBackStack(null);  // This line allows the user to navigate back to the ProfileFragment by pressing the back button.
+            transaction.commit();
+        });
+//        final TextView textView = binding.textProfile;
+//        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- GreenPlate/app/src/main/java/com/example/greenplate/ui/profile/ProfileViewModel.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/ui/profile/ProfileViewModel.java
@@ -6,15 +6,15 @@ import androidx.lifecycle.ViewModel;
 
 public class ProfileViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
-
-    public ProfileViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is profile fragment");
-
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
+//    private final MutableLiveData<String> mText;
+//
+//    public ProfileViewModel() {
+//        mText = new MutableLiveData<>();
+//        mText.setValue("This is profile fragment");
+//
+//    }
+//
+//    public LiveData<String> getText() {
+//        return mText;
+//    }
 }
