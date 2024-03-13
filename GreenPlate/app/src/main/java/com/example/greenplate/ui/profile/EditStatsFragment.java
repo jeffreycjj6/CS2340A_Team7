183aa2cb0f9ce78380d252cb47c580846cda22d2
Jayden-Causey <jcausey8@gatech.edu>
2024-03-12 21:31:19 -0400

    added setter for calorie goal

--- GreenPlate/app/src/main/java/com/example/greenplate/ui/profile/EditStatsFragment.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/ui/profile/EditStatsFragment.java
@@ -47,7 +47,7 @@ public class EditStatsFragment extends Fragment {
 
             if (!userHeight.equals("") && !userWeight.equals("") && !userGender.equals("")){
                 User user = User.getInstance();
-
+                
                 user.setHeight(Double.parseDouble(userHeight));
                 user.setWeight(Double.parseDouble(userWeight));
                 user.setGender(userGender);
