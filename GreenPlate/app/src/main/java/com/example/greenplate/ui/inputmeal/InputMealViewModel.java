452efc2c2859860dc25a16cf6e74776c1f68dc37
rhdmsdkk <rachelgekim@gmail.com>
2024-03-12 20:24:40 -0400

    changed calories in InputMealViewModel

--- GreenPlate/app/src/main/java/com/example/greenplate/ui/inputmeal/InputMealViewModel.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/ui/inputmeal/InputMealViewModel.java
@@ -10,10 +10,14 @@ public class InputMealViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
     User user = User.getInstance();
-    double target = 370 + 21.6 * (1 - (user.getWeight() / (Math.pow(user.getHeight(), 2)))
-            * user.getWeight());
+    double target = 0;
 
     public InputMealViewModel() {
+        if (user.getHeight() != 0 && user.getWeight() != 0) {
+            target = 370 + 21.6 * (1 - (user.getWeight() / (Math.pow(user.getHeight(), 2)))
+                    * user.getWeight());
+        }
+
         mText = new MutableLiveData<>();
         mText.setValue("At " + user.getHeight() + " meters tall and " + user.getWeight()
                 + " kilograms, your goal is " + target + " calories. You are at "
