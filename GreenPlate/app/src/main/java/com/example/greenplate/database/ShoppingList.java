cf8e551df94c79664e26d01ec7855a98c093fbe7
jessechen04 <chen.jesse04@gmail.com>
2024-04-16 19:31:05 -0400

    added shopping list class

---
+++ GreenPlate/app/src/main/java/com/example/greenplate/database/ShoppingList.java
@@ -0,0 +1,52 @@
+package com.example.greenplate.database;
+
+import java.util.ArrayList;
+
+public class ShoppingList {
+
+    private static ShoppingList shopping;
+    private ArrayList<Ingredient> shoppingList;
+
+    private ShoppingList() {
+        shoppingList = new ArrayList<>();
+    }
+
+    public static ShoppingList getInstance() {
+        if (shopping == null) {
+            shopping = new ShoppingList();
+        }
+        return shopping;
+    }
+
+    public static void resetInstance() {
+        shopping = null;
+    }
+
+    public ArrayList<Ingredient> getPantryList() {
+        return shoppingList;
+    }
+
+    public Ingredient getIngredient(String name) {
+        Ingredient ret = null;
+        for (int i = 0; i < shoppingList.size(); i++) {
+            Ingredient currIngredient = shoppingList.get(i);
+            if (currIngredient.getName().equals(name)) {
+                ret = currIngredient;
+            }
+        }
+        return ret;
+    }
+
+    public void addIngredient(Ingredient ingredient) {
+        shoppingList.add(ingredient);
+    }
+
+    public void removeIngredient(String name) {
+        for (int i = 0; i < shoppingList.size(); i++) {
+            Ingredient currIngredient = shoppingList.get(i);
+            if (currIngredient.getName().equals(name)) {
+                shoppingList.remove(currIngredient);
+            }
+        }
+    }
+}
