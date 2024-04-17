ba481458bea2b5306782675218b72567281babe7
Ignatius Djaynurdin <ignatius@gatech.edu>
2024-04-16 20:35:38 -0400

    Implement shopping page v2

--- GreenPlate/app/src/main/java/com/example/greenplate/ui/shoppinglist/AddShopFragment.java
+++ GreenPlate/app/src/main/java/com/example/greenplate/ui/shoppinglist/AddShopFragment.java
@@ -1,5 +1,6 @@
 package com.example.greenplate.ui.shoppinglist;
 
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -11,28 +12,81 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.greenplate.R;
+import com.example.greenplate.database.UserDatabase;
+import com.example.greenplate.databinding.FragmentAddShopBinding;
+import com.example.greenplate.databinding.FragmentInputIngredientBinding;
+import com.example.greenplate.ui.ingredient.IngredientsFragment;
+import com.example.greenplate.ui.ingredient.InputIngredientViewModel;
 
 public class AddShopFragment extends Fragment {
 
-    private AddShopViewModel mViewModel;
+    private FragmentAddShopBinding binding;
 
-    public static AddShopFragment newInstance() {
-        return new AddShopFragment();
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        AddShopViewModel addShopViewModel =
+                new ViewModelProvider(this).get(AddShopViewModel.class);
+
+        binding = FragmentAddShopBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        //final TextView textView = binding.textInputIngredient;
+        //inputIngredientViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        Button submit = binding.addShopListButton;
+        submit.setOnClickListener(v -> {
+            String ingredientName = binding.ingredientNameShop.getText().toString();
+            String quantityStr = binding.quantityShop.getText().toString();
+            String caloriesPerServingStr = binding.caloriesPerServingShop.getText().toString();
+            String expirationDate = binding.expirationDateShop.getText().toString();
+
+            if (!ingredientName.equals("") && !quantityStr.equals("")
+                    && !caloriesPerServingStr.equals("")) {
+
+                int quantity = Integer.parseInt(quantityStr);
+                int caloriesPerServing = Integer.parseInt(caloriesPerServingStr);
+
+                binding.ingredientNameShop.setText("");
+                binding.quantityShop.setText("");
+                binding.caloriesPerServingShop.setText("");
+                binding.expirationDateShop.setText("");
+
+                UserDatabase userDatabase = UserDatabase.getInstance();
+
+                if (expirationDate.equals("")) {
+                    userDatabase.writeNewIngredient(ingredientName, quantity, caloriesPerServing);
+                } else {
+                    userDatabase.writeNewIngredient(ingredientName, quantity, caloriesPerServing,
+                            expirationDate);
+                }
+
+                //user.setHeight(Double.parseDouble(userHeight));
+                //user.setWeight(Double.parseDouble(userWeight));
+                //user.setGender(userGender);
+                //
+                //UserDatabase database = UserDatabase.getInstance();
+                //database.writeHeightWeightGender(Double.parseDouble(userHeight),
+                //Double.parseDouble(userWeight), userGender);
+
+                ShoppingListFragment shoppingListFragment = new ShoppingListFragment();
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, shoppingListFragment);
+
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return root;
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_shop, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(AddShopViewModel.class);
-        // TODO: Use the ViewModel
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
 }
